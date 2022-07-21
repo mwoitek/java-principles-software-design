6c079a6f4933920e2d187c0a5dabe54c12fc8f73
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-21 16:07:21 -0300

    Week 1: Assignment 1

--- week_1/src/solution/EarthQuakeClient.java
+++ week_1/src/solution/EarthQuakeClient.java
@@ -3,23 +3,60 @@ package solution;
 import java.util.ArrayList;
 
 public class EarthQuakeClient {
-  public EarthQuakeClient() {
-    // TODO Auto-generated constructor stub
-  }
+
+  public EarthQuakeClient() {}
 
   public ArrayList<QuakeEntry> filterByMagnitude(ArrayList<QuakeEntry> quakeData, double magMin) {
     ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
-    // TODO
-
+    for (QuakeEntry qe : quakeData) {
+      if (qe.getMagnitude() > magMin) {
+        answer.add(qe);
+      }
+    }
     return answer;
+  }
+
+  public void bigQuakes(double magMin) {
+    EarthQuakeParser parser = new EarthQuakeParser();
+    String source = "../data/nov20quakedatasmall.atom";
+    ArrayList<QuakeEntry> list = parser.read(source);
+    System.out.println("read data for " + list.size() + " quakes");
+    list = filterByMagnitude(list, magMin);
+    for (QuakeEntry qe : list) {
+      System.out.println(qe);
+    }
+    System.out.println("Found " + list.size() + " quakes that match that criteria");
   }
 
   public ArrayList<QuakeEntry> filterByDistanceFrom(ArrayList<QuakeEntry> quakeData, double distMax,
       Location from) {
     ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
-    // TODO
-
+    for (QuakeEntry qe : quakeData) {
+      if (qe.getLocation().distanceTo(from) < distMax) {
+        answer.add(qe);
+      }
+    }
     return answer;
+  }
+
+  public void closeToMe() {
+    EarthQuakeParser parser = new EarthQuakeParser();
+    String source = "../data/nov20quakedatasmall.atom";
+    ArrayList<QuakeEntry> list = parser.read(source);
+    System.out.println("read data for " + list.size() + " quakes");
+
+    // This location is Durham, NC
+    // Location city = new Location(35.988, -78.907);
+    // This location is Bridgeport, CA
+    Location city = new Location(38.17, -118.82);
+
+    double distKm;
+    list = filterByDistanceFrom(list, 1000000.0, city);
+    for (QuakeEntry qe : list) {
+      distKm = qe.getLocation().distanceTo(city) / 1000.0;
+      System.out.println(distKm + " " + qe.getInfo());
+    }
+    System.out.println("Found " + list.size() + " quakes that match that criteria");
   }
 
   public void dumpCSV(ArrayList<QuakeEntry> list) {
@@ -28,43 +65,30 @@ public class EarthQuakeClient {
       System.out.printf("%4.2f,%4.2f,%4.2f,%s\n", qe.getLocation().getLatitude(),
           qe.getLocation().getLongitude(), qe.getMagnitude(), qe.getInfo());
     }
-
-  }
-
-  public void bigQuakes() {
-    EarthQuakeParser parser = new EarthQuakeParser();
-    // String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-    String source = "data/nov20quakedata.atom";
-    ArrayList<QuakeEntry> list = parser.read(source);
-    System.out.println("read data for " + list.size() + " quakes");
-
-  }
-
-  public void closeToMe() {
-    EarthQuakeParser parser = new EarthQuakeParser();
-    String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
-    ArrayList<QuakeEntry> list = parser.read(source);
-    System.out.println("read data for " + list.size() + " quakes");
-
-    // This location is Durham, NC
-    Location city = new Location(35.988, -78.907);
-
-    // This location is Bridgeport, CA
-    // Location city = new Location(38.17, -118.82);
-
-    // TODO
   }
 
   public void createCSV() {
     EarthQuakeParser parser = new EarthQuakeParser();
-    String source = "data/nov20quakedatasmall.atom";
-    // String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+    String source = "../data/nov20quakedatasmall.atom";
     ArrayList<QuakeEntry> list = parser.read(source);
     dumpCSV(list);
     System.out.println("# quakes read: " + list.size());
     for (QuakeEntry qe : list) {
       System.out.println(qe);
     }
+  }
+
+  //
+
+  public static void main(String[] args) {
+    EarthQuakeClient eqc = new EarthQuakeClient();
+
+    eqc.bigQuakes(5.0);
+    System.out.println();
+
+    eqc.closeToMe();
+
+    //
   }
 
 }
