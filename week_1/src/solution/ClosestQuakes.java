bb5316d129349ebc8b7fafc6c7106f8ef5496909
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-21 19:57:06 -0300

    Week 1: Assignment 4

--- week_1/src/solution/ClosestQuakes.java
+++ week_1/src/solution/ClosestQuakes.java
@@ -3,29 +3,62 @@ package solution;
 import java.util.ArrayList;
 
 public class ClosestQuakes {
+
+  private float[] computeDistances(ArrayList<QuakeEntry> quakeData, Location current) {
+    int numQuakes = quakeData.size();
+    float[] distances = new float[numQuakes];
+    for (int i = 0; i < numQuakes; i++) {
+      distances[i] = quakeData.get(i).getLocation().distanceTo(current);
+    }
+    return distances;
+  }
+
+  private int indexOfMin(float[] values) {
+    int idxMin = 0;
+    float min = values[0];
+    for (int idx = 1; idx < values.length; idx++) {
+      if (values[idx] < min) {
+        idxMin = idx;
+        min = values[idx];
+      }
+    }
+    return idxMin;
+  }
+
   public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location current,
       int howMany) {
-    ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
-    // TODO
-    return ret;
+    ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
+    float[] distances = computeDistances(quakeData, current);
+    int idx;
+    int numRepetitions = howMany <= quakeData.size() ? howMany : quakeData.size();
+    for (int i = 0; i < numRepetitions; i++) {
+      idx = indexOfMin(distances);
+      answer.add(quakeData.get(idx));
+      distances[idx] = Float.MAX_VALUE;
+    }
+    return answer;
   }
 
   public void findClosestQuakes() {
     EarthQuakeParser parser = new EarthQuakeParser();
-    // String source = "data/nov20quakedata.atom";
-    String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+    String source = "../data/nov20quakedatasmall.atom";
     ArrayList<QuakeEntry> list = parser.read(source);
-    System.out.println("read data for " + list.size());
-
+    System.out.println("read data for " + list.size() + " quakes");
     Location jakarta = new Location(-6.211, 106.845);
-
-    ArrayList<QuakeEntry> close = getClosest(list, jakarta, 10);
+    QuakeEntry qe;
+    double distanceInMeters;
+    ArrayList<QuakeEntry> close = getClosest(list, jakarta, 3);
     for (int k = 0; k < close.size(); k++) {
-      QuakeEntry entry = close.get(k);
-      double distanceInMeters = jakarta.distanceTo(entry.getLocation());
-      System.out.printf("%4.2f\t %s\n", distanceInMeters / 1000, entry);
+      qe = close.get(k);
+      distanceInMeters = jakarta.distanceTo(qe.getLocation());
+      System.out.printf("%4.2f\t %s\n", distanceInMeters / 1000, qe);
     }
     System.out.println("number found: " + close.size());
+  }
+
+  public static void main(String[] args) {
+    ClosestQuakes cq = new ClosestQuakes();
+    cq.findClosestQuakes();
   }
 
 }
