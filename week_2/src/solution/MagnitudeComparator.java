a6601d1e68d8aa2e7a4dd2a9431b5127bb145325
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-23 13:38:35 -0300

    week 2, part 2 setup

---
+++ week_2/src/solution/MagnitudeComparator.java
@@ -0,0 +1,11 @@
+package solution;
+
+import java.util.Comparator;
+
+public class MagnitudeComparator implements Comparator<QuakeEntry> {
+
+  public int compare(QuakeEntry qe1, QuakeEntry qe2) {
+    return Double.compare(qe1.getMagnitude(), qe2.getMagnitude());
+  }
+
+}
