01935a809e5b1810a768798d5e3859c74c526da6
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-23 14:35:57 -0300

    Week 2, Part 2: compareTo Method

--- week_2/src/solution/DifferentSorters.java
+++ week_2/src/solution/DifferentSorters.java
@@ -4,37 +4,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class DifferentSorters {
+
   public void sortWithCompareTo() {
     EarthQuakeParser parser = new EarthQuakeParser();
-    String source = "data/nov20quakedata.atom";
-    // String source =
-    // "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+    String source = "../data/nov20quakedata.atom";
     ArrayList<QuakeEntry> list = parser.read(source);
     Collections.sort(list);
     for (QuakeEntry qe : list) {
       System.out.println(qe);
     }
-
+    // int quakeNumber = 10;
+    // System.out.println("Print quake entry in position " + quakeNumber);
+    // System.out.println(list.get(quakeNumber));
   }
 
   public void sortByMagnitude() {
     EarthQuakeParser parser = new EarthQuakeParser();
-    String source = "data/nov20quakedata.atom";
-    // String source =
-    // "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+    String source = "../data/nov20quakedata.atom";
     ArrayList<QuakeEntry> list = parser.read(source);
     Collections.sort(list, new MagnitudeComparator());
     for (QuakeEntry qe : list) {
       System.out.println(qe);
     }
-
   }
 
   public void sortByDistance() {
     EarthQuakeParser parser = new EarthQuakeParser();
-    String source = "data/nov20quakedata.atom";
-    // String source =
-    // "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+    String source = "../data/nov20quakedata.atom";
     ArrayList<QuakeEntry> list = parser.read(source);
     // Location is Durham, NC
     Location where = new Location(35.9886, -78.9072);
@@ -42,6 +38,11 @@ public class DifferentSorters {
     for (QuakeEntry qe : list) {
       System.out.println(qe);
     }
-
   }
+
+  public static void main(String[] args) {
+    DifferentSorters ds = new DifferentSorters();
+    ds.sortWithCompareTo();
+  }
+
 }
--- week_2/src/solution/QuakeEntry.java
+++ week_2/src/solution/QuakeEntry.java
@@ -31,24 +31,32 @@ public class QuakeEntry implements Comparable<QuakeEntry> {
   }
 
   @Override
-  public int compareTo(QuakeEntry loc) {
-    double difflat = myLocation.getLatitude() - loc.myLocation.getLatitude();
-    if (Math.abs(difflat) < 0.001) {
-      double diff = myLocation.getLongitude() - loc.myLocation.getLongitude();
-      if (diff < 0)
-        return -1;
-      if (diff > 0)
-        return 1;
-      return 0;
+  public int compareTo(QuakeEntry qe) {
+    int c;
+    if ((c = Double.compare(magnitude, qe.getMagnitude())) != 0) {
+      return c;
     }
-    if (difflat < 0)
-      return -1;
-    if (difflat > 0)
-      return 1;
-
-    // never reached
-    return 0;
+    return Double.compare(depth, qe.getDepth());
   }
+
+  // @Override
+  // public int compareTo(QuakeEntry loc) {
+  // double difflat = myLocation.getLatitude() - loc.myLocation.getLatitude();
+  // if (Math.abs(difflat) < 0.001) {
+  // double diff = myLocation.getLongitude() - loc.myLocation.getLongitude();
+  // if (diff < 0)
+  // return -1;
+  // if (diff > 0)
+  // return 1;
+  // return 0;
+  // }
+  // if (difflat < 0)
+  // return -1;
+  // if (difflat > 0)
+  // return 1;
+  // // never reached
+  // return 0;
+  // }
 
   public String toString() {
     return String.format("(%3.2f, %3.2f), mag = %3.2f, depth = %3.2f, title = %s",
