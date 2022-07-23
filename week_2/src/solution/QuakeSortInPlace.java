f5a62272a311285b7ed11b3a06c0afb6cd620796
Marcio Woitek <mwoitekjr@gmail.com>
2022-07-22 23:15:01 -0300

    Week 2: Sort by Depth

--- week_2/src/solution/QuakeSortInPlace.java
+++ week_2/src/solution/QuakeSortInPlace.java
@@ -24,17 +24,6 @@ public class QuakeSortInPlace {
     }
   }
 
-  public void testSort() {
-    EarthQuakeParser parser = new EarthQuakeParser();
-    String source = "../data/nov20quakedatasmall.atom";
-    ArrayList<QuakeEntry> list = parser.read(source);
-    System.out.println("read data for " + list.size() + " quakes");
-    sortByMagnitude(list);
-    for (QuakeEntry qe : list) {
-      System.out.println(qe);
-    }
-  }
-
   public void dumpCSV(ArrayList<QuakeEntry> list) {
     System.out.println("Latitude,Longitude,Magnitude,Info");
     for (QuakeEntry qe : list) {
@@ -49,6 +38,46 @@ public class QuakeSortInPlace {
     ArrayList<QuakeEntry> list = parser.read(source);
     dumpCSV(list);
     System.out.println("# quakes read: " + list.size());
+  }
+
+  public int getLargestDepth(ArrayList<QuakeEntry> quakeData, int from) {
+    double depth;
+    int idxMax = from;
+    double maxDepth = quakeData.get(from).getDepth();
+    for (int idx = from + 1; idx < quakeData.size(); idx++) {
+      if ((depth = quakeData.get(idx).getDepth()) > maxDepth) {
+        idxMax = idx;
+        maxDepth = depth;
+      }
+    }
+    return idxMax;
+  }
+
+  public void sortByLargestDepth(ArrayList<QuakeEntry> in) {
+    int idxMax;
+    QuakeEntry quake;
+    QuakeEntry maxQuake;
+    for (int idx = 0; idx < in.size(); idx++) {
+      idxMax = getLargestDepth(in, idx);
+      quake = in.get(idx);
+      maxQuake = in.get(idxMax);
+      in.set(idx, maxQuake);
+      in.set(idxMax, quake);
+    }
+  }
+
+  public void testSort() {
+    EarthQuakeParser parser = new EarthQuakeParser();
+    String source = "../data/nov20quakedatasmall.atom";
+    ArrayList<QuakeEntry> list = parser.read(source);
+    System.out.println("read data for " + list.size() + " quakes");
+
+    // sortByMagnitude(list);
+    sortByLargestDepth(list);
+
+    for (QuakeEntry qe : list) {
+      System.out.println(qe);
+    }
   }
 
   public static void main(String[] args) {
